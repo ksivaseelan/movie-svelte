5f216336f52f6135b9757ddd421bdd93ca69fea2
ksivaseelan <ksivaseelan@gmail.com>
2021-08-26 19:43:22 -0400

    Movie details text clarity

--- src/routes/details/[slug].svelte
+++ src/routes/details/[slug].svelte
@@ -28,39 +28,43 @@
     style="background-image: url(https://image.tmdb.org/t/p/original{movie.backdrop_path});
             background-repeat: no-repeat;
             background-attachment: fixed;
-            background-size: cover;"
+            background-position: center;
+            background-size: cover;
+            "
     class="h-screen"
 >
-    <div
-        id="text-block"
-        class="flex flex-col justify-center items-center h-screen text-3xl font-bold backdrop-blur text-white"
-    >
-        <h1>{movie.title}</h1>
-        <br />
-        <p>{movie.overview}</p>
-        <br />
-        <p>
-            Release Date:
-            {movie.release_date.slice(8, 10)}
-            {movie.release_date.slice(5, 7)}
-            {movie.release_date.slice(0, 4)}
-        </p>
-        <p>Runtime: {movie.runtime} mins</p>
-        <br />
-        <p>Genres:</p>
-        {#each movie.genres as genres}
-            <li>{genres.name}</li>
-        {/each}
-        <br />
-        <p>Average Rating: {movie.vote_average}</p>
-        <p>Rating count from: {movie.vote_count}</p>
-        <br />
-        <p>Produced by:</p>
-        {#each movie.production_companies as prod}
-            <li>{prod.name} from {prod.origin_country}</li>
-        {/each}
-        <br />
-        <a href={movie.homepage}>Go to website</a>
+    <div class="bg-black bg-opacity-80">
+        <div
+            id="text-block"
+            class="flex flex-col justify-center items-center h-screen text-3xl font-bold text-white"
+        >
+            <h1 class="text-6xl">{movie.title}</h1>
+            <br />
+            <p>{movie.overview}</p>
+            <br />
+            <p>
+                Release Date:
+                {movie.release_date.slice(8, 10)},
+                {movie.release_date.slice(5, 7)},
+                {movie.release_date.slice(0, 4)}
+            </p>
+            <p>Runtime: {movie.runtime} mins</p>
+            <br />
+            <p>Genres:</p>
+            {#each movie.genres as genres}
+                <li>{genres.name}</li>
+            {/each}
+            <br />
+            <p>Average Rating: {movie.vote_average}</p>
+            <p>Rating count from: {movie.vote_count}</p>
+            <br />
+            <p>Produced by:</p>
+            {#each movie.production_companies as prod}
+                <li>{prod.name} from {prod.origin_country}</li>
+            {/each}
+            <br />
+            <a href={movie.homepage}>Go to website</a>
+        </div>
     </div>
 </div>
 
